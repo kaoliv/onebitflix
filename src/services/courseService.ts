842056471d577facdbb270fbfbdcb8e102da2d38
Kaique Oliveira <kaique.oliv72@gmail.com>
2024-01-27 18:54:12 -0300

    Obtendo 3 cursos em destaque

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -24,5 +24,22 @@ export const courseService = {
       }
     })
     return coursesWithEpisodes
+  },
+
+  getRandomFeaturedCourses:async () => {
+    const featuredCourses = await Course.findAll({
+      attributes: [
+        'id', 
+        'name',
+        'synopsis', 
+        ['thumbnail_url', 'thumbnailUrl']
+      ],
+      where: {
+        featured: true
+      }
+    })
+
+    const randomFeaturedCourses = featuredCourses.sort(() => 0.5 - Math.random())
+    return randomFeaturedCourses.slice(0, 3)
   }
 }
