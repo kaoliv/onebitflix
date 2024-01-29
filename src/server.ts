39b9e935624d5fa8b052e94d0b20bd181378dda0
kaique <kaique.oliv72@gmail.com>
2024-01-29 20:35:54 -0300

    Adicionando favoritos

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import { adminJs, adminJsRouter } from './adminjs'
 import { router } from './routes'
 
 const app = express()
-app.use(express.json())
 app.use(express.static('public'))
+app.use(express.json())
 app.use(adminJs.options.rootPath, adminJsRouter)
 app.use(router)
 
